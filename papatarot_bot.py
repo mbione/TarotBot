e0b54c4b1a15dcae58de01352df3023463c8ff99
mbione <61111297+mbione@users.noreply.github.com>
2020-02-17 04:07:30 -0300

    GitInit
    Adição das imagens e Código

---
+++ papatarot_bot.py
@@ -0,0 +1,81 @@
+import logging
+import random
+import telegram
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+updater = Updater("1034516978:AAHTZOJ39mZP7b8HEvjBNvVOP3Y4YBP0tHU", use_context=True)
+bot = telegram.Bot(token="1034516978:AAHTZOJ39mZP7b8HEvjBNvVOP3Y4YBP0tHU")
+# Get the dispatcher to register handlers
+dp = updater.dispatcher
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+Arcanos = [
+    ['https://raw.githubusercontent.com/mbione/papas-tarot/master/major00_Easy-Resize.com.jpg', '00 - O louco'],
+    ['https://raw.githubusercontent.com/mbione/papas-tarot/master/major01_Easy-Resize.com.jpg', '01 - O Mago'], 
+    ['https://raw.githubusercontent.com/mbione/papas-tarot/master/major02_Easy-Resize.com.jpg', '02 - A Sacerdotisa'],
+    ['https://raw.githubusercontent.com/mbione/papas-tarot/master/major03_Easy-Resize.com.jpg', '03 - A Imperatriz'],
+    ['https://raw.githubusercontent.com/mbione/papas-tarot/master/major04_Easy-Resize.com.jpg', '04 - O Imperador'],
+    ['https://miro.medium.com/max/1480/1*NgsNrrgPJQq7KiUiUpdOCQ.jpeg', '05 - O Hierofante'],
+    ['https://raw.githubusercontent.com/mbione/papas-tarot/master/major06_Easy-Resize.com.jpg', '06 - Os Amantes'],
+    ['https://raw.githubusercontent.com/mbione/papas-tarot/master/major07_Easy-Resize.com.jpg', '07 - O Carro'],
+    ['https://raw.githubusercontent.com/mbione/papas-tarot/master/major08_Easy-Resize.com.jpg', '08 - A Força'],
+    ['https://raw.githubusercontent.com/mbione/papas-tarot/master/major09_Easy-Resize.com.jpg', '09 - O Eremita'],
+    ['https://raw.githubusercontent.com/mbione/papas-tarot/master/major10_Easy-Resize.com.jpg', '10 - A Roda da Fortuna'],
+    ['https://raw.githubusercontent.com/mbione/papas-tarot/master/major11_Easy-Resize.com.jpg', '11 - A Justiça'],
+    ['https://raw.githubusercontent.com/mbione/papas-tarot/master/major12_Easy-Resize.com.jpg', '12 - O Enforcado'],
+    ['https://raw.githubusercontent.com/mbione/papas-tarot/master/major13_Easy-Resize.com.jpg', '13 - A Morte'],
+    ['https://raw.githubusercontent.com/mbione/papas-tarot/master/major14_Easy-Resize.com.jpg', '14 - A Temperança'],
+    ['https://raw.githubusercontent.com/mbione/papas-tarot/master/major15_Easy-Resize.com.jpg', '15 - O Diabo'],
+    ['https://raw.githubusercontent.com/mbione/papas-tarot/master/major16_Easy-Resize.com.jpg', '16 - A Torre'],
+    ['https://raw.githubusercontent.com/mbione/papas-tarot/master/major17_Easy-Resize.com.jpg', '17 - A Estrela'],
+    ['https://raw.githubusercontent.com/mbione/papas-tarot/master/major18_Easy-Resize.com.jpg', '18 - A Lua'],
+    ['https://raw.githubusercontent.com/mbione/papas-tarot/master/major19_Easy-Resize.com.jpg', '19 - O Sol'],
+    ['https://raw.githubusercontent.com/mbione/papas-tarot/master/major20_Easy-Resize.com.jpg', '20 - O Julgamento'],
+    ['https://raw.githubusercontent.com/mbione/papas-tarot/master/major21_Easy-Resize.com.jpg', '21 - O Mundo']
+    ]
+
+def start(update, context):
+
+    update.message.reply_text('Hi!')
+
+
+def dia(update, context):
+
+    random.shuffle(Arcanos)
+    update.message.reply_text(Arcanos[0][1])
+    #update.message.reply_text(Arcanos[0][0])
+    #bot.sendPhoto(chat_id, Arcanos[0][0], Arcanos[0][1])
+    chat_id = update.message.chat_id
+    bot.sendPhoto(chat_id, Arcanos[0][0], Arcanos[0][1])
+
+
+def error(update, context):
+
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+
+    #updater = Updater("1034516978:AAHTZOJ39mZP7b8HEvjBNvVOP3Y4YBP0tHU", use_context=True)
+    #dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("dia", dia))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__': 
+    main()
